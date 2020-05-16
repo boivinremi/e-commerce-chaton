4295a77eaf970924314367af8a217d4d6578267b
boivin.remi <boivin.remi@outlook.com>
2020-05-16 12:43:10 +0200

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
 
-  resources :charges
+  devise_for :users
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#home'
   get '/profil', to: 'static_pages#profil'
+
+  # Resources alphabetic sort as son as possible
+  resources :charges
 
   resources :carts, except: [:destroy] do
     get 'pay/:money', action: 'pay', as: 'payment'
     get '/edit/:item_id', action: 'edit', as: 'edit'
   end
 
-  # get '/carts/edit/:id_item_to_del', to: 'carts#edit'
-
   resources :items, only: [:new, :create] do
     get 'show', action: 'show', as: 'show'
     get 'add/', action: 'add', as: 'add'
   end
-
-  devise_for :users
 end
